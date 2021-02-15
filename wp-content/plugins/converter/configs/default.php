665222dc6b29468f312960716b78dd7418ad8efe
Vadim Malashenko <vadim.m@rgbcode.com>
2021-02-15 21:14:02 +1000

    Add config

---
+++ wp-content/plugins/converter/configs/default.php
@@ -0,0 +1,8 @@
+<?php
+
+return (object) [
+    'api' => (object) [
+        'url' => 'https://pro-api.coinmarketcap.com/v1/cryptocurrency',
+        'key' => 'cbfb6652-193d-4379-9e2f-c2ecf8c65369'
+    ]
+];
