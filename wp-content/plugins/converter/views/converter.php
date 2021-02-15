f0322acfce991b9e1bd86ace83ef3b7a0e14ebe3
Vadim Malashenko <vadim.m@rgbcode.com>
2021-02-15 21:18:24 +1000

    Add view

---
+++ wp-content/plugins/converter/views/converter.php
@@ -0,0 +1,4 @@
+<div class="container" id="<?php echo $converter->id; ?>" style="min-width:300px">
+    <div class="currencies row row-cols-2"></div>
+    <div class="pairs row row-cols-1 mt-4"></div>
+</div>
